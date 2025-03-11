8e086ca42bb78b558ab3909d92e4a389c31ac76c
cidney54 <cidney.petit-saint.1@ens.etsmtl.ca>
2025-03-11 19:37:41 -0400

    modification jeu.c

--- jeu.c
+++ jeu.c
@@ -1,13 +1,34 @@
 //
 // Created by stephane on 08/01/25.
 //
+#include <interaction.h>
+#include <terrain.h>
 
 //  ***********************************
 //  Definitions des fonctions publiques
 //  ***********************************
 
 // Definir la fonction 'jeu_maj_carburant_joueur' ici
+/**
+ * @brief Met a jour la quantite de carburant du joueur apres un deplacement. Annule la quantite de carburant sur la case du joueur.
+ * @name jeu_maj_carburant_joueur
+ * @param joueur_ligne la ligne ou se trouve maintenant le joueur
+ * @param joueur_colonne la colonne ou se trouve maintenant le joueur
+ * @param joueur_carburant l'adresse ou placer la nouvelle quantite de carburant du joueur apres deplacement
+ * @param terrain le terrain sur lequel le joueur s'est deplace
+ */
+void jeu_maj_carburant_joueur(int joueur_ligne, int joueur_colonne, int joueur_carburant,
+                              int terrain[NB_LIGNES][NB_COLONNES]) {
+    terrain_set_carburant(terrain,joueur_ligne,joueur_colonne,joueur_carburant);
 
+    int i,j;
+    while(joueur_ligne && joueur_colonne == terrain[i][j]){
+
+        joueur_carburant +=
+    }
+
+
+}
 // Definir la fonction 'jeu_deplacer_joueur' ici
 
 // Definir la fonction 'jeu_init' ici
--- terrain.c
+++ terrain.c
@@ -8,31 +8,32 @@
 #include "terrain.h"
 #include <stdbool.h>
 
+
 //  ***********************************
 //  Definitions des fonctions publiques
 //  ***********************************
 
 // Definir la fonction 'terrain_generer_position_sortie' ici
 
-void terrain_generer_position_sortie(int *sortie_ligne,int *sortie_colonne) {
+void terrain_generer_position_sortie(int *sortie_ligne, int *sortie_colonne) {
 
     /* Générer et enregistrer les coordonnées de sortie aléatoire. */
-    *sortie_ligne = nb_aleatoire(0,NB_LIGNES-1);
-    *sortie_colonne = nb_aleatoire(0,NB_COLONNES-1);
+    *sortie_ligne = nb_aleatoire(0, NB_LIGNES - 1);
+    *sortie_colonne = nb_aleatoire(0, NB_COLONNES - 1);
 }
 
 // Definir la fonction 'terrain_generer_position_depart' ici
 
 void terrain_generer_position_depart(int destination_ligne, int destination_colonne, int *depart_ligne,
-    int *depart_colonne) {
+                                     int *depart_colonne) {
 
     /* Boucle de génération de coordonnées aléatoires tant que la distance « Manhattan » entre les coordonnées de
      * départ et de sortie n'est pas plus grande que dix. */
     do {
 
         /* Générer et enregistrer les coordonnées de sortie aléatoire. */
-        *depart_ligne = nb_aleatoire(0,NB_LIGNES-1);
-        *depart_colonne = nb_aleatoire(0,NB_COLONNES-1);
+        *depart_ligne = nb_aleatoire(0, NB_LIGNES - 1);
+        *depart_colonne = nb_aleatoire(0, NB_COLONNES - 1);
 
     } while (abs(destination_ligne - *depart_ligne) + abs(destination_colonne - *depart_colonne) <= 10);
 }
@@ -46,8 +47,7 @@ int terrain_contient(int position_ligne, int position_colonne) {
     if (position_ligne < 0 || position_colonne < 0 || position_ligne >= NB_LIGNES || position_colonne >= NB_COLONNES) {
 
         return false;
-    }
-    else {
+    } else {
         return true;
     }
 }
@@ -64,7 +64,7 @@ int terrain_get_carburant(int terrain[NB_LIGNES][NB_COLONNES], int position_lign
 // Definir la fonction 'terrain_set_carburant' ici
 
 void terrain_set_carburant(int terrain[NB_LIGNES][NB_COLONNES], int position_ligne, int position_colonne,
-    int carburant) {
+                           int carburant) {
 
     /* Enregistrer la quantité de carburant dans la case en paramètre. */
     terrain[position_ligne][position_colonne] = carburant;
@@ -102,31 +102,33 @@ void terrain_creer_stations_carburant(int terrain[NB_LIGNES][NB_COLONNES], int q
             for (int j = 0; j < NB_COLONNES; j++) {
 
                 /* Vérifier que la case n'est pas déjà rempli. */
-                if (terrain [i][j] == 0) {
+                if (terrain[i][j] == 0) {
 
                     /* Créer un nombre aléatoire dans l'intervalle [1,100]. */
-                    int ratio_zero = nb_aleatoire(1,100);
+                    int ratio_zero = nb_aleatoire(1, 100);
 
                     /* Remplir le tableau du carburant en limitant les cases potentielles jusqu'à que la quantité
                      * globale de carburant disponible est moins que le maximum qui peut être déposé dans une case. */
                     if (ratio_zero > 99 && quantite_globale > STATION_MAX_CARBURANT) {
 
-                        carburant = nb_aleatoire(0,STATION_MAX_CARBURANT); // Générer une quantité aléatoire de carburant.
-                        terrain_set_carburant(terrain,i,j,carburant); // Enregistrer le carburant dans la case.
+                        carburant = nb_aleatoire(0,
+                                                 STATION_MAX_CARBURANT); // Générer une quantité aléatoire de carburant.
+                        terrain_set_carburant(terrain, i, j, carburant); // Enregistrer le carburant dans la case.
                         quantite_globale -= carburant; // Soustraire le carburant de la quantité totale.
 
                     }
-                    /* Remplir le tableau avec le carburant restant jusqu'à la quantité est nulle. */
+                        /* Remplir le tableau avec le carburant restant jusqu'à la quantité est nulle. */
                     else if (ratio_zero > 99 && quantite_globale <= STATION_MAX_CARBURANT) {
 
                         /* Générer une quantité de carburant tant qu'elle ne dépasse pas la quantité disponible. */
                         do {
 
-                            carburant = nb_aleatoire(0,STATION_MAX_CARBURANT); // Générer une quantité aléatoire de carburant.
+                            carburant = nb_aleatoire(0,
+                                                     STATION_MAX_CARBURANT); // Générer une quantité aléatoire de carburant.
 
                         } while (quantite_globale - carburant < 0);
 
-                        terrain_set_carburant(terrain,i,j,carburant); // Enregistrer le carburant dans la case.
+                        terrain_set_carburant(terrain, i, j, carburant); // Enregistrer le carburant dans la case.
                         quantite_globale -= carburant; // Soustraire le carburant déposé de la quantité total.
                     }
                 }
@@ -137,22 +139,21 @@ void terrain_creer_stations_carburant(int terrain[NB_LIGNES][NB_COLONNES], int q
 
 // Definir la fonction 'terrain_afficher' ici
 
-void terrain_afficher(int terrain[NB_LIGNES][NB_COLONNES],int *courante_ligne, int *courante_colonne,
-    int destination_ligne,int destination_colonne) {
+void terrain_afficher(int terrain[NB_LIGNES][NB_COLONNES], int *courante_ligne, int *courante_colonne,
+                      int destination_ligne, int destination_colonne) {
 
     /* Afficher le tableau. */
     /* Parcourir chaque ligne du tableau en incluant les lignes de séparation. */
-    for (int i = -1; i < NB_LIGNES*2; i++) {
+    for (int i = -1; i < NB_LIGNES * 2; i++) {
 
         /* Parcourir chaque colonne du tableau. */
         for (int j = 0; j < NB_COLONNES; j++) {
 
             /* Vérifier si la ligne agit d'une ligne de séparation ou de valeurs du tableau. */
-            if ( i % 2 != 0) {
+            if (i % 2 != 0) {
 
                 printf("+---"); // Afficher les caractères pour la ligne de sépraration.
-            }
-            else {
+            } else {
 
                 if (i / 2 == *courante_ligne && j == *courante_colonne) {
 
@@ -160,8 +161,7 @@ void terrain_afficher(int terrain[NB_LIGNES][NB_COLONNES],int *courante_ligne, i
 
                     // couleur_set(STYLE_BOLD,FG_RED,BG_BLACK); // Changer la couleur de la case de position actuel du joueur.
                     printf(" X ");
-                }
-                else if (i / 2 == destination_ligne && j == destination_colonne) {
+                } else if (i / 2 == destination_ligne && j == destination_colonne) {
 
 
                     printf("|");
@@ -169,10 +169,9 @@ void terrain_afficher(int terrain[NB_LIGNES][NB_COLONNES],int *courante_ligne, i
                     // couleur_set(STYLE_BOLD,FG_BLUE,BG_BLACK); // Changer la couleur de la case destination du joeur.
 
                     printf(" Y ");
-                }
-                else {
+                } else {
 
-                    printf("| %d ",terrain[i/2][j]); // Afficher la valeur correspondante du tableau.
+                    printf("| %d ", terrain[i / 2][j]); // Afficher la valeur correspondante du tableau.
                 }
             }
         }
@@ -183,8 +182,7 @@ void terrain_afficher(int terrain[NB_LIGNES][NB_COLONNES],int *courante_ligne, i
         if (i % 2 != 0) {
 
             printf("+\n"); // Effectuer un retour à la prochaine ligne de valeurs du tableau.
-        }
-        else {
+        } else {
 
             printf("|\n"); // Effectuer un retour à la prochaine ligne de séparation.
         }
