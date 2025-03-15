e774d7a0d36109b0f5373bc651290b15e506862d
ItzSkyLab <skyscream444@gmail.com>
2025-03-15 15:14:45 -0400

    Update dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -52,6 +52,7 @@ void initialiser_visitees(t_visite visitees);
  * @return true si la case a deja ete visitee, false sinon
  */
 //  Ecrire le prototype de la fonction 'est_case_visitee' ici
+bool est_case_visitee(t_visites visitees, int case_ligne, int case_colonne);
 
 /**
  * @brief Recherche la case non visitee qui possede le plus petit cout.
@@ -159,7 +160,10 @@ void initialiser_visitees(t_visite visitees){
     }
   }
 }
+
 // Definir la fonction 'est_case_visitee' ici
+bool est_case_visitee(t_visites visitees, int case_ligne, int case_colonne); {
+    return visitees [case_ligne][case_colonne]; // Permet de suivre les cases du terrain que l'algo a deja explorees(chaque cases  est marquees comme visitee ou non)
 
 // Definir la fonction 'choisir_min_dist_non_visitee' ici
 
